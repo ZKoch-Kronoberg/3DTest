5ad8214de550ac40ea247d094e4f7577cdb68bdd
ZKoch-Kronoberg <165011923+ZKoch-Kronoberg@users.noreply.github.com>
2024-05-13 15:55:46 +0200

    Add site header

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>
-        3D-Test!
-      </h1>
+      <header class="bg-gradient-to-r from-krono-lime via-krono-green to-krono-pink py-4">
+        <div class="container mx-auto">
+          <h1 class="text-white text-3xl font-bold text-left">3D-Test</h1>
+        </div>
+      </header>
       <Viewer/>
     </>
-    
   );
 }
 
