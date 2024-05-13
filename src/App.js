3b494b81d4c4eacf76fe52595173aab921262d7c
ZKoch-Kronoberg <165011923+ZKoch-Kronoberg@users.noreply.github.com>
2024-05-13 12:34:16 +0200

    Move viewer code into Viewer component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import './App.css';
-import { useEffect } from 'react';
-import SceneInit from './lib/SceneInit';
-import * as THREE from 'three'
-import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+import Viewer from './Viewer';
+
 
 function App() {
-  useEffect(() => {
-    const scene = new SceneInit('c');
-    scene.initialize();
-    scene.animate();
 
-    /* //just some box for testing
-    const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
-    const boxMaterial = new THREE.MeshNormalMaterial();
-    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-    scene.scene.add(boxMesh); */
-
-    const gltfLoader = new GLTFLoader();
-    gltfLoader.load(`${process.env.PUBLIC_URL}/models/tracheostomy_tube_cleaned.glb`,(gltfScene) =>{
-      scene.scene.add(gltfScene.scene);
-    })
-  }, []);
-  
   return (
-    <div>
-      <canvas id='c'/>
-    </div>
+    <Viewer/>
   );
 }
 
---
+++ src/Viewer.js
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import SceneInit from './lib/SceneInit';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+
+function Viewer() {
+  useEffect(() => {
+    const scene = new SceneInit('c');
+    scene.initialize();
+    scene.animate();
+
+    /* //just some box for testing
+    const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
+    const boxMaterial = new THREE.MeshNormalMaterial();
+    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    scene.scene.add(boxMesh); */
+
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load(`${process.env.PUBLIC_URL}/models/tracheostomy_tube_cleaned.glb`,(gltfScene) =>{
+      scene.scene.add(gltfScene.scene);
+    })
+  }, []);
+  
+  return (
+    <div>
+      <canvas id='c'/>
+    </div>
+  );
+}
+
+export default Viewer;
