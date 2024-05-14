9e79fe573aa19aad8ed1eab5e83e4b73ad31a69a
ZKoch-Kronoberg <165011923+ZKoch-Kronoberg@users.noreply.github.com>
2024-05-14 14:40:51 +0200

    Add text content

--- src/App.js
+++ src/App.js
@@ -6,12 +6,37 @@ function App() {
 
   return (
     <>
-      <header class="bg-gradient-to-r from-krono-lime via-krono-green to-krono-pink py-4">
-        <div class="container mx-auto">
-          <h1 class="text-white text-3xl font-bold text-left">3D-Test</h1>
+      <header className="bg-gradient-to-r from-krono-lime via-krono-green to-krono-pink py-4">
+        <div>
+          <h1 className="text-white text-3xl font-bold text-left">3D-Test</h1>
         </div>
       </header>
-      <Viewer/>
+      <main className="mt-2 px-2 w-full">
+        <h2 className='text-2xl mt-2'>
+          Om denna sidan
+        </h2>
+        <p>
+          Denna sidan gjordes för att experimentera med 3D-skanning och webbaserad 3D-visning med syfta att undersöka om vi på Region Kronoberg vill använda tekniken på något sätt.
+        </p>
+        <p>
+          Den enda modellen som finns tillgänglig just nu är en 3D-skann utav en tracheostomi-apparat som skannades med androidappen <a href='https://play.google.com/store/apps/details?id=ai.polycam'>Polycam</a> i sammarbete med KTC och sedan städades upp i programmet <a href='https://www.blender.org/'>Blender</a>
+        </p>
+        <h2 className='text-2xl mt-2'>
+          Instruktioner
+        </h2>
+        <ol className='list-inside list-decimal pl-2'>
+          <li>
+            Låt 3D-modellen ladda. Detta kan ta upp till en minut på vissa
+            enheter men går snabbare på de flesta.
+          </li>
+          <li>
+            Placera muspekaren innanför visningsområdet. 3D-visaren kommer
+            annars inte kunna uppfatta dina gester.
+          </li>
+
+        </ol>
+        <Viewer/>
+      </main>
     </>
   );
 }
