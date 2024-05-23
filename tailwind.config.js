2f383ee5ac8e86dd8d270e30a1f689c1f357cc2b
ZKoch-Kronoberg <165011923+ZKoch-Kronoberg@users.noreply.github.com>
2024-05-23 11:20:17 +0200

    Revert "Add Kronoberg's primary colors to tailwind config" and revert "add logo"
    I missed these when i was removing Region Kronoberg's branding from the project
    
    Revert "Add Kronoberg's primary colors to tailwind config"
    
    This reverts commit 9c8420c28ce802b262e7c1c0ccf41d4902739aac.
    
    Revert "Add logo"
    
    This reverts commit 0bb6192bd74e3a3c92f6f5926154ff4899a0a526.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        'krono': {
-          lime: '#83b81a',
-          green: '#006633',
-          pink: '#e13288',
-          yellow: 'ffd300',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [
     require('tailwind-scrollbar-hide')
